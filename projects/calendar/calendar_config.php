190c5b58d340e670fd3ca6e95a435e3088abbd13
mozilla@kewis.ch <mozilla@kewis.ch@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-11-08 22:41:18 +0000

    Calendar: Main Page updates
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.org/trunk@97540 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ projects/calendar/calendar_config.php
@@ -0,0 +1,11 @@
+<?php
+
+// This file is not used everywhere yet, please use it!
+
+$lightningReleaseShort = "1.0";
+$lightningReleaseLong = "1.0";
+
+$sunbirdReleaseLong = "1.0 beta1";
+$sunbirdReleaseShort = "1.0b1";
+
+?>
